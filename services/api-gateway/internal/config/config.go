7c75e2e795a920d3cab86510743e30781997ac76
Jacob Hunsaker <jacob.hunsaker96@gmail.com>
2026-05-21 12:52:49 -0500

    update default values

--- services/api-gateway/internal/config/config.go
+++ services/api-gateway/internal/config/config.go
@@ -6,38 +6,38 @@ import (
 )
 
 type Config struct {
-	Port               string
-	Env                string
-	LogServiceURL      string
-	AnalysisServiceURL string
-	AIServiceURL       string
-	FrontendURL        string
-	WCLClientID        string
-	WCLClientSecret    string
-	WCLAuthorizeURL    string
-	WCLTokenURL        string
-	WCLRedirectURL     string
-	RedisURL             string
-	DatabaseURL          string
-	StripeSecretKey      string
-	StripeWebhookSecret  string
-	StripeProPriceID     string
+	Port                string
+	Env                 string
+	LogServiceURL       string
+	AnalysisServiceURL  string
+	AIServiceURL        string
+	FrontendURL         string
+	WCLClientID         string
+	WCLClientSecret     string
+	WCLAuthorizeURL     string
+	WCLTokenURL         string
+	WCLRedirectURL      string
+	RedisURL            string
+	DatabaseURL         string
+	StripeSecretKey     string
+	StripeWebhookSecret string
+	StripeProPriceID    string
 }
 
 func Load() Config {
 	return Config{
-		Port:               firstEnv("PORT", "API_GATEWAY_PORT", "8080"),
-		Env:                getEnv("ENV", "development"),
-		LogServiceURL:      getEnv("LOG_SERVICE_URL", "http://localhost:8081"),
-		AnalysisServiceURL: getEnv("ANALYSIS_SERVICE_URL", "http://localhost:8082"),
-		AIServiceURL:       getEnv("AI_SERVICE_URL", "http://localhost:8083"),
-		FrontendURL:        getEnv("FRONTEND_URL", "http://localhost:5173"),
-		WCLClientID:        firstEnv("WCL_CLIENT_ID", "WARCRAFTLOGS_CLIENT_ID", ""),
-		WCLClientSecret:    firstEnv("WCL_CLIENT_SECRET", "WARCRAFTLOGS_CLIENT_SECRET", ""),
-		WCLAuthorizeURL:    getEnv("WCL_AUTHORIZE_URL", "https://www.warcraftlogs.com/oauth/authorize"),
-		WCLTokenURL:        getEnv("WCL_TOKEN_URL", "https://www.warcraftlogs.com/oauth/token"),
-		WCLRedirectURL:     getEnv("WCL_REDIRECT_URL", "http://localhost:8080/api/auth/callback"),
-		RedisURL:            getEnv("REDIS_URL", ""),
+		Port:                firstEnv("PORT", "API_GATEWAY_PORT", "8080"),
+		Env:                 getEnv("ENV", "development"),
+		LogServiceURL:       getEnv("LOG_SERVICE_URL", "http://localhost:8081"),
+		AnalysisServiceURL:  getEnv("ANALYSIS_SERVICE_URL", "http://localhost:8082"),
+		AIServiceURL:        getEnv("AI_SERVICE_URL", "http://localhost:8083"),
+		FrontendURL:         getEnv("FRONTEND_URL", "http://localhost:5173"),
+		WCLClientID:         getEnv("WCL_CLIENT_ID", ""),
+		WCLClientSecret:     getEnv("WCL_CLIENT_SECRET", ""),
+		WCLAuthorizeURL:     getEnv("WCL_AUTHORIZE_URL", "https://www.warcraftlogs.com/oauth/authorize"),
+		WCLTokenURL:         getEnv("WCL_TOKEN_URL", "https://www.warcraftlogs.com/oauth/token"),
+		WCLRedirectURL:      getEnv("WCL_REDIRECT_URL", "http://localhost:8080/api/auth/callback"),
+		RedisURL:            getEnv("REDIS_URL", "redis://localhost:6379"),
 		DatabaseURL:         buildDatabaseURL(),
 		StripeSecretKey:     getEnv("STRIPE_SECRET_KEY", ""),
 		StripeWebhookSecret: getEnv("STRIPE_WEBHOOK_SECRET", ""),
